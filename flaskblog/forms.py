1b59b69390575217c9aeca74e2adfbd66cb4cac4
unknown <githinvgeorge@gmail.com>
2019-02-15 22:45:16 +0530

    profile pic change

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
+from flask_wtf.file import  FileField,FileAllowed
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flaskblog.models import User,Post
@@ -27,3 +29,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    picture = FileField('Update Picture',validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username already taken. PLease take another name')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email already taken. PLease take another email')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template,url_for,flash,redirect,request
+import secrets,os
 from flaskblog import app,db,bcrypt
-from flaskblog.forms import LoginForm,RegistrationForm
+from flaskblog.forms import LoginForm,RegistrationForm,UpdateAccountForm
 from flaskblog.models import User,Post
 from flask_login import login_user,logout_user,current_user,login_required
 
@@ -70,12 +71,37 @@ def login():
             flash('Please check the email and password','danger')
     return render_template('login.html',title='Login Page', form=form)
 
+
 @app.route('/logout')
 def logout():
    logout_user()
    return redirect(url_for('login'))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
+@app.route('/account',methods=['GET','POST'])
 @login_required
 def account():
-    return render_template('account.html',title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Account updated successfully','success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static',filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html',title="Account", image_file=image_file, form=form)
